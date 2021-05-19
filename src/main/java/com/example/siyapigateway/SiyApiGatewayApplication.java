b3e4d84c7ebddc99b1f6f48c7eb74737ae87dd13
JeongJin984 <philipwq3963@gmail.com>
2021-05-19 19:15:19 +0900

    'm'

--- src/main/java/com/example/siyapigateway/SiyApiGatewayApplication.java
+++ src/main/java/com/example/siyapigateway/SiyApiGatewayApplication.java
@@ -2,10 +2,12 @@ package com.example.siyapigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaClient
+@EnableCaching
 public class SiyApiGatewayApplication {
 
     public static void main(String[] args) {
